99aae940038482c0fc47c07c1db69d269467a989
Harman arora <arora29harman@gmail.com>
2024-03-08 23:54:48 +0530

    updates

--- src/components/Medium/HomeMain.jsx
+++ src/components/Medium/HomeMain.jsx
@@ -8,10 +8,9 @@ export const HomeMain = () => {
     animate={{ opacity: 1, y: "0" }}
     exit={{ opacity: 0, y: "-50%" }}
     transition={transition1} className="h-[calc(100vh-70px)] flex items-center justify-center flex-col">
-        <div className="headin md:w-[60vw] w-[90vw]   z-10">
+        <div className="headin md:w-[60vw] w-[90vw] z-10">
           <h1 
-          
-          className="text-6xl md:text-7xl text-center  font-extrabold tracking-tight ">
+          className="text-5xl md:text-6xl text-center  font-extrabold tracking-tight ">
             Start coding{" "}
             <span className=" leading-tight bg-gradient-to-r from-white via-[#9372FF] to-[#4B69FF] text-transparent mr-3 bg-clip-text">
               instantly
@@ -20,7 +19,7 @@ export const HomeMain = () => {
           </h1>
         </div>
         <div className="w-[80%]  z-10">
-          <p className="mt-6 mb-0 text-zinc-500 text-center text-xl">
+          <p className="mt-6 mb-0 text-zinc-300 text-center text-xl">
             Social Learning and expert-led programs to start, transition or
             boost your career
           </p>
